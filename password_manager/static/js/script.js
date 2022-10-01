cff86a9a081510be24ba4c6840ec1d70045c8b66
iljah <ilja200303@gmail.com>
2022-10-01 10:13:14 +0300

    if icon doest't exist replace this icon to first letter of app name

---
+++ password_manager/static/js/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function () {
+    var allGs = document.getElementsByTagName('g');
+    if (allGs) {
+        Array.from(allGs).forEach(element => {
+            if (element.parentElement) {
+
+                // console.log(element.parentElement.nextSibling.nextSibling.nextSibling);
+                element.parentElement.nextSibling.nextSibling.nextSibling.style.display = "inline"
+                element.parentElement.style.display = "none"
+            }
+
+        });
+    }
+})
+
