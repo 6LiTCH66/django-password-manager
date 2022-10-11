e4de55e16f5fa25f81b06c50cc324f3ef537ad7a
iljah <ilja200303@gmail.com>
2022-10-11 21:27:49 +0300

    deploying app on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn django_password_manager.wsgi
--- django_password_manager/settings.py
+++ django_password_manager/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'django_password_manager.urls'
@@ -139,3 +143,7 @@ LOGOUT_REDIRECT_URL = '/login'
 
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/user/signup'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_on_heroku.settings(locals())
